d9adbfeb9ccd5e34cf3ae2b236e0680cc4aaf20d
Douglas Santos <douglasrosasantos@gmail.com>
2025-05-26 18:49:50 -0300

    reset css and set new variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { TimerIcon } from 'lucide-react'
 import { Heading } from './components/Heading'
 import './styles/theme.css'
 
 export function App() {
     return (
         <>
-            <Heading />
+            <Heading>
+                Olá mundo
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Hello World</p>
         </>
     )
