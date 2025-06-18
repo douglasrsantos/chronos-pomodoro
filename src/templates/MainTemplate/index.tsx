62b63a461e9e844f9ba2adcba6be7933eb3e4bfa
Douglas Santos <douglasrosasantos@gmail.com>
2025-06-18 07:31:52 -0300

    created NotFound and AboutPomodoro pages and GenericHtml styles

--- src/templates/MainTemplate/index.tsx
+++ src/templates/MainTemplate/index.tsx
@@ -1,5 +1,3 @@
-import './styles/theme.css'
-import './styles/global.css'
 import { Container } from '../../components/Container'
 import { Logo } from '../../components/Logo'
 import { Menu } from '../../components/Menu'
