1846d4b7d87e06d552d8d667eb5b44b38a59c432
Douglas Santos <douglasrosasantos@gmail.com>
2025-09-15 11:09:18 -0300

    added title on pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,13 @@ import { MainTemplate } from '../../templates/MainTemplate'
 import { Container } from '../../components/Container'
 import { CountDown } from '../../components/CountDown'
 import { MainForm } from '../../components/MainForm'
+import { useEffect } from 'react'
 
 export function Home() {
+    useEffect(() => {
+        document.title = 'Entenda a Técnica Pomodoro - Chronos Pomodoro';
+    }, [])
+
     return (
         <MainTemplate>
             <Container>
